d695c8ad425ef57a3f7855ea1470bb6f1ad446ef
Souptik Samanta <161435060+souptik-samanta@users.noreply.github.com>
2024-11-18 00:24:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 function scrollToSection(sectionId) {
   const section = document.getElementById(sectionId);
-  if (section) {
-    section.scrollIntoView({ behavior: 'smooth' });
-  }
+  section.scrollIntoView({ behavior: 'smooth' });
 }
 
 function updateTimeAndGreeting() {
   const timeElement = document.getElementById("time-display");
   const greetingElement = document.getElementById("greeting");
-
   const now = new Date();
   const hours = now.getHours();
   const minutes = String(now.getMinutes()).padStart(2, "0");
-
   timeElement.textContent = `Time: ${hours}:${minutes}`;
-
   if (hours < 12) {
     greetingElement.textContent = "Good Morning";
   } else if (hours < 18) {
@@ -23,6 +18,5 @@ function updateTimeAndGreeting() {
     greetingElement.textContent = "Good Evening";
   }
 }
-
 setInterval(updateTimeAndGreeting, 1000);
 updateTimeAndGreeting();
